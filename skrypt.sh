057f3833cf8e3426f1ff6979c8f3fea5fdf5b7a7
majaszturmaj <maja.szturmaj@gmail.com>
2022-04-22 15:33:09 +0200

    Skrypt z opcją utworzenia 100 plików logX.txt.

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ "$1" = '--date' ]]; then
-  date
+if [[ "$1" = '--logs' ]]; then
+  for ((i = 1 ; i < 101 ; i++)); do
+	 var=$(date)
+	 echo "log$i data.sh $var" >> log$i.txt
+  done
 fi
